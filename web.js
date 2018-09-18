0f311a13c4914e2a5b5eccda16223e4bfcac0d66
Vitaliy Matiyash <vitaliy.matiyash@alliancedata.com>
2018-09-18 00:10:08 -0400

    express

--- web.js
+++ web.js
@@ -10,6 +10,7 @@ app.use(express.static(__dirname + '/webroot'));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(bodyParser.json());
 
 app.get('/', function(request, response) {
   response.sendfile(__dirname + '/webroot/index.html');
